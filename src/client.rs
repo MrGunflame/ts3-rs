7bc97e24445d2ef6cf8e613784a8ac1c6ba996fd
0xc0001a2040 <mrgunflame@protonmail.com>
2021-01-28 23:01:30 +0100

    Update main files

--- src/client.rs
+++ src/client.rs
@@ -1,162 +1,275 @@
-use std::io::{Read, Write};
-use std::net::SocketAddr;
-use std::net::TcpStream;
+use bytes::Bytes;
+use std::collections::HashMap;
+use std::convert::From;
+use std::error;
+use std::fmt::{self, Display, Formatter};
+use std::io;
 use std::result;
+use std::str::from_utf8;
+use std::time::Duration;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::TcpStream;
+use tokio::net::ToSocketAddrs;
+use tokio::sync::{mpsc, oneshot};
+use tokio::task::spawn;
+use tokio::time::sleep;
 
-struct Error {
-    id: u32,
-    text: String,
+pub type Result<T> = result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    IO(io::Error),
+    TS3 { id: usize, msg: String },
+    SendError,
 }
 
-type Result<T> = result::Result<T, Error>;
-
-pub enum ConnectMode {
-    TCP,
-    SSH,
-}
-
-pub struct Client {
-    mode: ConnectMode,
-    stream: Option<TcpStream>,
-    addr: String,
-}
-
-pub struct Whoami {
-    virtualserver_status: String,
-    virtualserver_unique_identifier: String,
-    virtualserver_port: u16,
-    virtualserver_id: u32,
-    client_id: u32,
-    client_channel_id: u32,
-    client_nickname: String,
-    client_database_id: u32,
-    client_login_name: String,
-    client_unique_identifier: String,
-    client_origin_server_id: u8,
-}
-
-impl Whoami {
-    fn from(vec: Vec<DecodedField>) -> Whoami {
-        let (
-            virtualserver_status,
-            virtualserver_unique_identifier,
-            client_nickname,
-            client_login_name,
-            client_unique_identifier,
-        ): (String, String, String, String, String);
-        let (
-            virtualserver_port,
-            virtualserver_id,
-            client_id,
-            cient_channel_id,
-            client_database_id,
-            client_origin_server_id,
-        ): (u16, u32, u32, u32, u32, u8);
-
-        for f in DecodedField {
-            match f.name {}
+impl Error {
+    fn ok(&self) -> bool {
+        use Error::*;
+        match self {
+            TS3 { id, msg: _ } => *id == 0,
+            _ => false,
         }
-
-        Whoami {}
     }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        use Error::*;
+        write!(
+            f,
+            "{}",
+            match self {
+                IO(err) => format!("{}", err),
+                TS3 { id, msg } => format!("TS3 Error {}: {}", id, msg),
+                SendError => "SendError".to_owned(),
+            }
+        )
+    }
+}
+
+impl error::Error for Error {}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::IO(err)
+    }
+}
+
+// Read a error from a raw server response
+impl From<RawResp> for Error {
+    fn from(mut resp: RawResp) -> Error {
+        Error::TS3 {
+            id: resp.items[0]
+                .remove("id")
+                .unwrap()
+                .unwrap()
+                .parse()
+                .unwrap(),
+            msg: resp.items[0].remove("msg").unwrap().unwrap(),
+        }
+    }
+}
+
+struct Cmd {
+    bytes: Bytes,
+    resp: oneshot::Sender<Result<RawResp>>,
+}
+
+/// A Client used to send commands to the serverquery interface.
+#[derive(Clone)]
+pub struct Client {
+    tx: mpsc::Sender<Cmd>,
 }
 
 impl Client {
-    pub fn new(addr: A) -> Client {
-        Client {
-            mode: ConnectMode::TCP,
-            stream: None,
-            addr: addr,
-        }
+    /// Create a new connection
+    pub async fn new<A: ToSocketAddrs>(addr: A) -> Result<Client> {
+        let (tx, mut rx) = mpsc::channel::<Cmd>(32);
+
+        let stream = TcpStream::connect(addr).await?;
+
+        let (reader, mut writer) = stream.into_split();
+        let mut reader = BufReader::new(reader);
+
+        // read_tx and read_rx are used to communicate between the read and the write
+        // thread
+        let (read_tx, mut read_rx) = mpsc::channel(32);
+
+        // Read task
+        spawn(async move {
+            loop {
+                let mut buf = Vec::new();
+                if let Err(err) = reader.read_until(b'\r', &mut buf).await {
+                    println!("{}", err);
+                    continue;
+                }
+
+                // Remove the last two bytes '\n' and '\r'
+                buf.truncate(buf.len() - 2);
+
+                let resp = RawResp::from(buf.as_slice());
+                match resp.is_error() {
+                    true => {
+                        let _ = read_tx.send((RawResp::new(), Error::from(resp))).await;
+                    }
+                    false => {
+                        // Read another line
+                        buf.clear();
+                        if let Err(err) = reader.read_until(b'\r', &mut buf).await {
+                            eprintln!("{}", err);
+                            continue;
+                        }
+
+                        let err = RawResp::from(buf.as_slice());
+                        let _ = read_tx.send((resp, Error::from(err))).await;
+                    }
+                }
+            }
+        });
+
+        spawn(async move {
+            while let Some(cmd) = rx.recv().await {
+                // Write the command string
+                if let Err(err) = writer.write(&cmd.bytes).await {
+                    let _ = cmd.resp.send(Err(err.into()));
+                    continue;
+                }
+
+                // Write a '\n' to send the command
+                if let Err(err) = writer.write(&[b'\n']).await {
+                    let _ = cmd.resp.send(Err(err.into()));
+                    continue;
+                }
+
+                // Wait for the response from the reader thread
+                let (resp, err) = read_rx.recv().await.unwrap();
+
+                let _ = cmd.resp.send(match err.ok() {
+                    true => Ok(resp),
+                    false => Err(err),
+                });
+            }
+        });
+
+        // Keepalive loop
+        let tx2 = tx.clone();
+        spawn(async move {
+            loop {
+                let tx = tx2.clone();
+                sleep(Duration::from_secs(60)).await;
+                {
+                    let (resp_tx, _) = oneshot::channel();
+                    if let Err(_) = tx
+                        .send(Cmd {
+                            bytes: Bytes::from_static("version".as_bytes()),
+                            resp: resp_tx,
+                        })
+                        .await
+                    {}
+                }
+            }
+        });
+
+        Ok(Client { tx })
     }
 
-    fn rw(&mut self, buf: &[u8]) -> Result<()> {
-        self.stream.as_ref().unwrap().write(buf).unwrap();
+    async fn send(&self, cmd: String) -> Result<RawResp> {
+        let tx = self.tx.clone();
 
+        let (resp_tx, resp_rx) = oneshot::channel();
+        match tx
+            .send(Cmd {
+                bytes: Bytes::from(cmd.into_bytes()),
+                resp: resp_tx,
+            })
+            .await
+        {
+            Ok(_) => {
+                let resp = resp_rx.await;
+                resp.unwrap()
+            }
+            Err(_) => Err(Error::SendError),
+        }
+    }
+}
+
+// TS3 Commands go here
+impl Client {
+    /// Authenticate with the given data.
+    pub async fn login(&self, username: &str, password: &str) -> Result<()> {
+        self.send(format!(
+            "login client_login_name={} client_login_password={}",
+            username, password
+        ))
+        .await?;
         Ok(())
     }
 
-    pub fn login(username: String, password: String) {}
+    /// Switch to the virtualserver (voice) with the given server id
+    pub async fn use_sid(&self, sid: usize) -> Result<()> {
+        self.send(format!("use sid={}", sid)).await?;
+        Ok(())
+    }
 
-    pub fn whoami() {}
-
-    pub fn servergroupaddclient(sgid: u32, cldbid: u32) -> Result<()> {
+    /// Send a quit command, disconnecting the client and closing the TCP connection
+    pub async fn quit(&self) -> Result<()> {
+        self.send("quit".to_owned()).await?;
         Ok(())
     }
 }
 
-#[derive(Debug, PartialEq)]
-struct DecodedField {
-    pub name: String,
-    pub value: String,
+struct RawResp {
+    items: Vec<HashMap<String, Option<String>>>,
 }
 
-impl DecodedField {
-    fn new() -> DecodedField {
-        DecodedField {
-            name: "".to_owned(),
-            value: "".to_owned(),
+impl RawResp {
+    fn new() -> RawResp {
+        RawResp { items: Vec::new() }
+    }
+
+    // Returns whether the decoded response is the server error response
+    // This is true when a key named "error" exists in the map
+    fn is_error(&self) -> bool {
+        match self.items.get(0) {
+            Some(map) => map.contains_key("error"),
+            None => false,
         }
     }
-
-    fn push_name(&mut self, s: &str) {
-        self.name.push_str(s);
-    }
-
-    fn push_value(&mut self, s: &str) {
-        self.value.push_str(s);
-    }
 }
 
-// read_buf reads the buffer into a vec of key-value pairs
-fn read_buf(buf: &[u8]) -> Vec<DecodedField> {
-    let mut decf: Vec<DecodedField> = Vec::new();
-    decf.push(DecodedField::new());
+impl From<&[u8]> for RawResp {
+    fn from(buf: &[u8]) -> RawResp {
+        let mut items = Vec::new();
 
-    let mut val = false;
-    for b in buf {
-        match b {
-            0x20 => {
-                val = false;
-                decf.push(DecodedField::new());
+        // Split all items lists into separate strings first
+        // If the content is no list a single item is remained
+        let res: Vec<&str> = from_utf8(&buf).unwrap().split("|").collect();
+        for entry in res {
+            let mut map = HashMap::new();
+
+            // All key-value pairs are separated by ' '
+            let res: Vec<&str> = entry.split(" ").collect();
+            for item in res {
+                // Each pair that contains a '=' has both a key and a value
+                // A pair that has no '=' is only a key
+                // Only split the first '=' as splitting multiple could split strings inside the value
+                let parts: Vec<&str> = item.splitn(2, "=").collect();
+
+                // Insert key and value when both exist
+                // Otherwise None is inserted with the key
+                map.insert(
+                    parts.get(0).unwrap().to_string(),
+                    match parts.len() {
+                        n if n > 1 => Some(parts.get(1).unwrap().to_string()),
+                        _ => None,
+                    },
+                );
             }
-            0x3D => {
-                val = true;
-            }
-            _ => {
-                let mut last = decf.last_mut().unwrap();
-                match val {
-                    false => last.push_name(std::str::from_utf8(&[*b]).unwrap()),
-                    true => last.push_value(std::str::from_utf8(&[*b]).unwrap()),
-                };
-            }
+
+            items.push(map);
         }
-    }
 
-    decf
-}
-
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_read_buf() {
-        assert_eq!(
-            read_buf(String::from("virt=0 test=1 ok").as_bytes()),
-            vec![
-                DecodedField {
-                    name: "virt".to_owned(),
-                    value: "0".to_owned(),
-                },
-                DecodedField {
-                    name: "test".to_owned(),
-                    value: "1".to_owned(),
-                },
-                DecodedField {
-                    name: "ok".to_owned(),
-                    value: "".to_owned(),
-                },
-            ]
-        );
+        RawResp { items }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,21 @@
-// mod client;
-use std::convert::From;
-use std::io::{self, BufRead, BufReader};
-use std::net::{TcpStream, ToSocketAddrs};
-use std::result;
+//! # ts3
+//! A WIP ts3 query interface library
+//!
+//! # Examples
+//!
+//! ```rust
+//! #[tokio::main]
+//! async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+//!     // Create a new client and connect to the server query interface
+//!     let client = Client::new("localhost:10011").await?;
+//!
+//!     // switch to virtual server with id 1
+//!     client.use_sid(1).await?;
+//!
+//!     Ok(())
+//! }
+//! ```
 
-struct Error {
-    io: Option<std::io::Error>,
-}
+mod client;
 
-impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Self {
-        Self { io: Some(err) }
-    }
-}
-
-type Result<T> = result::Result<T, Error>;
-
-trait TSEncoded {
-    fn from(buf: &[u8]) -> Self;
-    fn into() -> [u8];
-}
-
-// pub use client::Client;
-
-struct Client {
-    rx: BufReader<TcpStream>,
-    tx: TcpStream,
-}
-
-impl Client {
-    fn new<A: ToSocketAddrs>(addr: A) -> Result<Client> {
-        let (tx, rx) = Self::new_stream(addr)?;
-        Ok(Client { tx, rx })
-    }
-
-    fn new_stream<A: ToSocketAddrs>(addr: A) -> Result<(TcpStream, BufReader<TcpStream>)> {
-        let sock = TcpStream::connect(addr)?;
-
-        let mut reader = BufReader::new(sock.try_clone()?);
-        let mut buf = Vec::new();
-        reader.read_until(b'\r', &mut buf)?;
-
-        buf.clear();
-
-        Ok((sock, reader))
-    }
-
-    fn read(&mut self) {}
-
-    // fn write<T: AsRef<str>>(&mut self, cmd: T) {
-    //     writeln!(&mut self.tx, "{}", cmd);
-    // }
-
-    // fn quit(&mut self) -> Result<()> {
-    //     writeln!(&mut self.tx, "quit")?;
-    // }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub use client::Client;
