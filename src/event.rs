642d041e9177dd2decce4514e4bc5da5fa2e7f5e
MrGunflame <mrgunflame@protonmail.com>
2021-03-31 16:29:21 +0200

    Remove unused dispatch_event function

--- src/event.rs
+++ src/event.rs
@@ -103,30 +103,6 @@ impl Client {
     }
 }
 
-// Dispatch the event to the appropriate handler method. This function will stay alive as
-// long as the handler method executes, so this should be moved to a separate task.
-pub(crate) async fn dispatch_event(c: Client, resp: RawResp, event: &str) {
-    let c2 = c.clone();
-    let handler = c2.inner.read().unwrap().handler.clone();
-
-    match event {
-        "notifycliententerview" => handler.cliententerview(c, resp),
-        // "notifyclientleftview" => handler.clientleftview(c, resp),
-        "notifyserveredited" => handler.serveredited(c, resp),
-        "notifychanneldescriptionchanged" => handler.channeldescriptionchanged(c, resp),
-        "notifychannelpasswordchanged" => handler.channelpasswordchanged(c, resp),
-        "notifychannelmoved" => handler.channelmoved(c, resp),
-        "notifychanneledited" => handler.channeledited(c, resp),
-        "notifychannelcreated" => handler.channelcreated(c, resp),
-        "notifychanneldeleted" => handler.channeldeleted(c, resp),
-        "notifyclientmoved" => handler.clientmoved(c, resp),
-        "notifytextmessage" => handler.textmessage(c, resp.into()),
-        "notifytokenused" => handler.tokenused(c, resp),
-        _ => unreachable!(),
-    }
-    .await;
-}
-
 /// All events sent by the server will be dispatched to their appropriate trait method.
 /// In order to receive events you must subscribe to the events you want to receive using servernotifyregister.
 #[async_trait]
