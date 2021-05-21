f3c58d78f4552f2ccabd650128f8d2faaa8b179e
MrGunflame <mrgunflame@protonmail.com>
2021-05-21 16:30:07 +0200

    Add DecodeError, update Decode trait to use BoxError

--- ts3/src/lib.rs
+++ ts3/src/lib.rs
@@ -20,22 +20,73 @@
 
 pub mod client;
 pub mod event;
+mod macros;
 
 pub use client::{Client, RawResp};
 pub use event::EventHandler;
+pub use ts3_derive::Decode;
 
 use std::convert::TryFrom;
 use std::fmt::Debug;
 use std::fmt::{self, Display, Formatter};
 use std::io;
-use std::num::ParseIntError;
 use std::str::{from_utf8, FromStr};
-use std::string::FromUtf8Error;
-pub use ts3_derive::Decode;
+use std::error;
 
 pub enum ParseError {
     InvalidEnum,
 }
+
+type BoxError = Box<dyn error::Error + Sync + Send>;
+
+#[derive(Debug)]
+pub enum Error {
+    /// Error returned from the ts3 interface. id of 0 indicates no error.
+    TS3 { id: u16, msg: String },
+    /// Io error from the underlying tcp stream.
+    Io(io::Error),
+    /// Error occured while decoding the server response.
+    Decode(BoxError),
+    SendError,
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::Io(err)
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            Self::TS3 { id, msg } => write!(f, "TS3 Error {}: {}", id, msg),
+            Self::Io(err) => write!(f, "Io Error: {}", err),
+            Self::Decode(err) => write!(f, "Error decoding value: {}", err),
+            Self::SendError => write!(f, "Failed to send command, channel closed"),
+        }
+    }
+}
+
+impl error::Error for Error {}
+
+#[derive(Debug)]
+pub enum DecodeError {
+    UnexpectedEof,
+    UnexpectedChar(char),
+    UnexpectedByte(u8),
+}
+
+impl Display for DecodeError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            Self::UnexpectedEof => write!(f, "Unexpected end while decoding"),
+            Self::UnexpectedChar(c) => write!(f, "Unexpected char decoding: {}", c),
+            Self::UnexpectedByte(b) => write!(f, "Unexpected byte decoding: {}", b),
+        }
+    }
+}
+
+impl error::Error for DecodeError {}
 
 /// A list of other objects that are being read from or written to the TS3 server interface.
 /// It implements both `FromStr` and `ToString` as long as `T` itself also implements these traits.
@@ -109,9 +160,184 @@ where
     }
 }
 
+/// Any type implementing `Decode` can be directly decoded from the TS3 stream.
+/// It provides the complete buffer of the response from the stream.
+pub trait Decode<T> {
+    fn decode(buf: &[u8]) -> Result<T, BoxError>;
+}
+
+/// Implement `Decode` for `Vec` as long as `T` itself also implements `Decode`.
+impl<T> Decode<Vec<T>> for Vec<T>
+where
+    T: Decode<T>,
+{
+    fn decode(buf: &[u8]) -> Result<Vec<T>, BoxError> {
+        // Create a new vec and push all items to it.
+        // Items are separated by a '|' char and no space before/after.
+        let mut list = Vec::new();
+        for b in buf.split(|c| *c == b'|') {
+            list.push(T::decode(&b)?);
+        }
+        Ok(list)
+    }
+}
+
+/// The `impl_decode` macro implements `Decode` for any type that implements `FromStr`.
+macro_rules! impl_decode {
+    ($t:ty) => {
+        impl Decode<$t> for $t {
+            fn decode(buf: &[u8]) -> std::result::Result<$t, BoxError> {
+                Ok(from_utf8(buf)?.parse()?)
+            }
+        }
+    };
+}
+
+/// Implement `Decode` for `()`. Calling `()::decode(&[u8])` will never fail
+/// and can always be unwrapped safely.
+impl Decode<()> for () {
+    fn decode(_: &[u8]) -> Result<(), BoxError> {
+        Ok(())
+    }
+}
+
+// Implement `Decode` for `String`
+impl Decode<String> for String {
+    fn decode(buf: &[u8]) -> Result<String, BoxError> {
+        // Create a new string, allocating the same length as the buffer. Most
+        // chars are one-byte only.
+        let mut string = String::with_capacity(buf.len());
+
+        // Create a peekable iterator to iterate over all bytes, appending all bytes
+        // and replacing escaped chars.
+        let mut iter = buf.into_iter().peekable();
+        while let Some(b) = iter.next() {
+            match b {
+                // Match any escapes, starting with a '\' followed by another char.
+                b'\\' => {
+                    match iter.peek() {
+                        Some(c) => match c {
+                            b'\\' => string.push('\\'),
+                            b'/' => string.push('/'),
+                            b's' => string.push(' '),
+                            b'p' => string.push('|'),
+                            b'a' => string.push(7u8 as char),
+                            b'b' => string.push(8u8 as char),
+                            b'f' => string.push(12u8 as char),
+                            b'n' => string.push(10u8 as char),
+                            b'r' => string.push(13u8 as char),
+                            b't' => string.push(9u8 as char),
+                            b'v' => string.push(11u8 as char),
+                            _ => return Err(Box::new(DecodeError::UnexpectedByte(**c))),
+                        },
+                        None => return Err(Box::new(DecodeError::UnexpectedEof)),
+                    }
+                    iter.next();
+                }
+                _ => string.push(char::try_from(*b).unwrap()),
+            }
+        }
+
+        // Shrink the string to its fitting size before returning it.
+        string.shrink_to_fit();
+        Ok(string)
+    }
+}
+
+impl Decode<bool> for bool {
+    fn decode(buf: &[u8]) -> Result<bool, BoxError> {
+        match buf.get(0) {
+            Some(b) => match b {
+                b'0' => Ok(true),
+                b'1' => Ok(false),
+                _ => Err(Box::new(DecodeError::UnexpectedByte(*b))),
+            },
+            None => Err(Box::new(DecodeError::UnexpectedEof)),
+        }
+    }
+}
+
+// Implement `Decode` for all int types.
+impl_decode!(isize);
+impl_decode!(i8);
+impl_decode!(i16);
+impl_decode!(i32);
+impl_decode!(i64);
+impl_decode!(i128);
+
+impl_decode!(usize);
+impl_decode!(u8);
+impl_decode!(u16);
+impl_decode!(u32);
+impl_decode!(u64);
+impl_decode!(u128);
+
+impl Decode<Error> for Error {
+    fn decode(buf: &[u8]) -> Result<Error, BoxError> {
+        let (mut id, mut msg) = (0, String::new());
+
+        // Error is a key-value map separated by ' ' with only the id and msg key.
+        for s in buf.split(|c| *c == b' ') {
+            // Error starts with "error" as the first key.
+            if s == b"error" {
+                continue;
+            }
+
+            // Get both key and value from the buffer, separated by a '='.
+            let parts: Vec<&[u8]> = s.splitn(2, |c| *c == b'=').collect();
+
+            match parts.get(0) {
+                Some(key) => {
+                    // Extract the value.
+                    let val = match parts.get(1) {
+                        Some(val) => val,
+                        None => return Err(Box::new(DecodeError::UnexpectedEof)),
+                    };
+
+                    // Match the key of the pair and assign the corresponding value.
+                    match *key {
+                        b"id" => {
+                            id = u16::decode(val)?;
+                        },
+                        b"msg" => {
+                            msg = String::decode(val)?;
+                        }
+                        _ => (),
+                    }
+                }
+                None => return Err(Box::new(DecodeError::UnexpectedEof)),
+            }
+        }
+
+        Ok(Error::TS3 { id, msg })
+    }
+}
+
 mod tests {
-    use super::List;
     use std::str::FromStr;
+    use super::*;
+
+    #[test]
+    fn test_vec_decode() {
+        let buf = b"test|test2";
+        assert_eq!(Vec::<String>::decode(buf).unwrap(), vec!["test".to_owned(), "test2".to_owned()]);
+    }
+
+    #[test]
+    fn test_string_decode() {
+        let buf = b"Hello\\sWorld!";
+        assert_eq!(String::decode(buf).unwrap(), "Hello World!".to_owned());
+    }
+
+    #[test]
+    fn test_error_decode() {
+        let buf = b"error id=0 msg=ok";
+        let (id, msg) = match Error::decode(buf).unwrap() {
+            Error::TS3 { id, msg } => (id, msg),
+            _ => unreachable!(),
+        };
+        assert!(id == 0 && msg == "ok".to_owned());
+    }
 
     #[test]
     fn test_list_to_string() {
@@ -132,197 +358,5 @@ mod tests {
                 items: vec![1, 2, 3, 4]
             }
         );
-    }
-}
-
-/// A type implementing `Decode` allows to be read from the TS stream
-pub trait Decode<T> {
-    type Err: Debug;
-
-    fn decode(buf: &[u8]) -> Result<T, Self::Err>;
-}
-
-// Implement `Decode` for `Vec<T>` if T implements `Decode`
-impl<T> Decode<Vec<T>> for Vec<T>
-where
-    T: Decode<T>,
-{
-    type Err = T::Err;
-
-    fn decode(buf: &[u8]) -> Result<Vec<T>, Self::Err> {
-        // Create a new vec and push all items to it
-        // Items are separated by a '|' char and no space before/after
-        let mut list = Vec::new();
-        for b in buf.split(|c| *c == b'|') {
-            list.push(T::decode(&b)?);
-        }
-        Ok(list)
-    }
-}
-
-/// The `impl_decode` macro implements `Decode` for any type that implements `FromStr`.
-#[macro_export]
-macro_rules! impl_decode {
-    ($t:ty) => {
-        impl Decode<$t> for $t {
-            type Err = std::num::ParseIntError;
-
-            fn decode(buf: &[u8]) -> std::result::Result<$t, Self::Err> {
-                from_utf8(buf).unwrap().parse()
-            }
-        }
-    };
-}
-
-// Implement `Decode` for `()`. Calling `()::decode(&[u8])` will never fail.
-impl Decode<()> for () {
-    type Err = ();
-
-    fn decode(_: &[u8]) -> Result<(), ()> {
-        Ok(())
-    }
-}
-
-// Implement `Decode` for `String`
-impl Decode<String> for String {
-    type Err = std::string::FromUtf8Error;
-
-    fn decode(buf: &[u8]) -> Result<String, Self::Err> {
-        let mut string = String::with_capacity(buf.len());
-
-        let mut iter = buf.into_iter().peekable();
-        while let Some(b) = iter.next() {
-            match b {
-                b'\\' => {
-                    match iter.peek() {
-                        Some(c) => match c {
-                            b'\\' => string.push('\\'),
-                            b'/' => string.push('/'),
-                            b's' => string.push(' '),
-                            b'p' => string.push('|'),
-                            b'a' => string.push(7u8 as char),
-                            b'b' => string.push(8u8 as char),
-                            b'f' => string.push(12u8 as char),
-                            b'n' => string.push(10u8 as char),
-                            b'r' => string.push(13u8 as char),
-                            b't' => string.push(9u8 as char),
-                            b'v' => string.push(11u8 as char),
-                            _ => unreachable!(),
-                        },
-                        None => unreachable!(),
-                    }
-                    iter.next();
-                }
-                _ => string.push(char::try_from(*b).unwrap()),
-            }
-        }
-
-        Ok(string)
-    }
-}
-
-impl Decode<bool> for bool {
-    type Err = Error;
-
-    fn decode(buf: &[u8]) -> Result<bool, Self::Err> {
-        match buf.get(0) {
-            Some(b) => match b {
-                b'0' => Ok(true),
-                b'1' => Ok(false),
-                _ => panic!("Unexpected char decoding bool: {}", b),
-            },
-            None => panic!("Unexpected end decoding bool"),
-        }
-    }
-}
-
-// Implement all integer types
-impl_decode!(isize);
-impl_decode!(i8);
-impl_decode!(i16);
-impl_decode!(i32);
-impl_decode!(i64);
-impl_decode!(i128);
-
-impl_decode!(usize);
-impl_decode!(u8);
-impl_decode!(u16);
-impl_decode!(u32);
-impl_decode!(u64);
-impl_decode!(u128);
-
-#[derive(Debug)]
-pub enum Error {
-    IO(io::Error),
-    TS3 { id: u16, msg: String },
-    SendError,
-    ParseIntError(ParseIntError),
-    Utf8Error(FromUtf8Error),
-}
-
-impl std::error::Error for Error {}
-
-impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Error {
-        Error::IO(err)
-    }
-}
-
-impl From<ParseIntError> for Error {
-    fn from(err: ParseIntError) -> Error {
-        Error::ParseIntError(err)
-    }
-}
-
-impl From<FromUtf8Error> for Error {
-    fn from(err: FromUtf8Error) -> Error {
-        Error::Utf8Error(err)
-    }
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        use Error::*;
-        write!(
-            f,
-            "{}",
-            match self {
-                IO(err) => format!("{}", err),
-                TS3 { id, msg } => format!("TS3 Error {}: {}", id, msg),
-                SendError => "SendError".to_owned(),
-                ParseIntError(err) => format!("{}", err),
-                Utf8Error(err) => format!("{}", err),
-            }
-        )
-    }
-}
-
-impl Decode<Error> for Error {
-    type Err = Error;
-
-    fn decode(buf: &[u8]) -> Result<Error, Error> {
-        let (mut id, mut msg) = (0, String::new());
-
-        for s in buf.split(|c| *c == b' ') {
-            let parts: Vec<&[u8]> = s.splitn(2, |c| *c == b'=').collect();
-
-            match *parts.get(0).unwrap() {
-                b"id" => {
-                    id = match u16::decode(parts.get(1).unwrap()) {
-                        Ok(id) => id,
-                        Err(err) => return Err(err.into()),
-                    }
-                }
-                b"msg" => {
-                    msg = match String::decode(parts.get(1).unwrap()) {
-                        Ok(msg) => msg,
-                        Err(err) => return Err(err.into()),
-                    }
-                }
-                _ => (),
-            }
-        }
-
-        Ok(Error::TS3 { id, msg })
     }
 }
